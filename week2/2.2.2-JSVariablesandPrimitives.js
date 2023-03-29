623b8207c9e1150d93c80df12eb175aeabfa1a25
eat-a-p3ach <lyncyyhodge@gmail.com>
2023-03-28 21:58:47 -0500

    JS Variables and Primitives part 2

---
+++ week2/2.2.2-JSVariablesandPrimitives.js
@@ -0,0 +1,33 @@
+// create a variable called "name" that references a string
+let name="Theodore";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong="Come as you are";
+// create a variable called "wage" that references a number
+let wage="74";
+// create a variable called "age" that references a number
+let age="30";
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = true;
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = false;
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong= "Smells Like Teen Spirit"
+console.log(typeof favoriteSong);
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing= "I like booleans more"
+// Happy Birthday!
+// Reassign "age" to a new value
+age= 31
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage= 100
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied= true
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("Hello World, my favorite song is " + (favoriteSong) + " and I am " + (wage))
