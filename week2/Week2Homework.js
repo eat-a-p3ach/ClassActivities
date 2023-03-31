137b5b7155982fd909ff35c5a1ec26ee3d96e5d6
eat-a-p3ach <lyncyyhodge@gmail.com>
2023-03-31 13:44:08 -0500

    Week 2 homework updates

--- week2/Week2Homework.js
+++ week2/Week2Homework.js
@@ -3,16 +3,19 @@ let pizzaPlace = "Pizza Planet";
 let numberOfToppings = 100;
 
 //6
-console.log(pizzaPlace); console.log(numberOfToppings);
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
 
 //7
 console.log(`"Check out ${pizzaPlace} and get up to ${numberOfToppings} toppings on your pizza!"`)
 
 //8
 if (numberOfToppings < 10) {
-  console.log("Quality, not quantity.")
+  console.log("Quality, not quantity.");
 } else if (numberOfToppings >= 10) {
-  console.log("A whole lot of pizza.")
+  console.log("A whole lot of pizza.");
 }
 
 //bonus challenge
