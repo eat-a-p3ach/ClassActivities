792b0066252e3e6b16d18f12c936588eaa7c9718
eat-a-p3ach <lyncyyhodge@gmail.com>
2023-04-09 16:19:40 -0500

    section 3

---
+++ week3/3.1.3-ReturnsAndShortCircuiting.js
@@ -0,0 +1,37 @@
+// ? what does the function below return?
+// function myNumberFunction() {
+//   let num = 2 ** 2;
+//   //4
+//   num = num + 3;
+//   //7
+//   num = num % 4;
+//   //3
+//   num = num * 14;
+//   //43
+//   console.log(num);
+// }
+
+// change the function above to return a value
+
+// Short Circuiting
+// ! the console.log will not print
+// function shortCircuitFunction() {
+//   return "Zap! - Short Circuited!";
+//   console.log(
+//     "This will never print because it is after the return statement."
+//   );
+// }
+
+// // ? will the following function short circuit?
+// let height = 50;
+// function checkCircusRides() {
+//   if (height < 48) {
+//     return "Take a ride on the carousel!";
+//   }
+//   if (height < 60) {
+//     return "Try the bumper cars!";
+//   }
+//   return "Enjoy the roller coaster!";
+// }
+
+// change the function above to use short circuiting
